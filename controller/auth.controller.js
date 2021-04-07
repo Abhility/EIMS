d1634465537273651f90c3e0d8279588afaf189e
Abhility <m1050754@mindtree.com>
2021-04-07 19:25:25 +0530

    changed file formatting

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -11,7 +11,13 @@ const loginController = async (req, res, next) => {
       if (user.password === password) {
         const token = generateToken({ id: user._id, role: user.role });
         res.setHeader('Authorization', token);
-        res.status(200).json({ message: 'Login successfull!', token, userId: user._id});
+        res.status(200).json({
+          message: 'Login successfull!',
+          token,
+          userId: user._id,
+          name: user.name,
+          role: user.role,
+        });
       } else {
         res.status(400).json({ message: 'invalid password' });
       }
